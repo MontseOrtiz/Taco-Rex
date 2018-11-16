2e94ed3fc5bbfed3b72d7bc2052fdd7cc23cf2e8
MontseOrtiz <montse_2908@hotmail.com>
2018-11-15 21:17:55 -0600

    Mejoras, dos jugadores con score

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ var images = {
 var obstaculos = [];
 var tacos = [];
 var audioBack = new Audio()
-var velocidad = 1
+// var velocidad = 1
 var btnPlay = document.getElementById('jugar')
 var btnInst = document.getElementById('instrucciones')
 var turnos = []
@@ -43,12 +43,12 @@ function Board(){
     this.height = canvas.height
     this.image = new Image()
     this.image.src = images.bg
-    // this.velocidad= 1
+    this.velocidad= 1
     this.draw = function(){
         if(Math.random()<1-(Math.pow(.9,frames/1000000))){
-            velocidad+=2
+            this.velocidad+=2
         }
-        this.x -= velocidad
+        this.x -= this.velocidad
         if(this.x < - this.width) this.x = 0
         ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
         ctx.drawImage(this.image,this.x + this.width,this.y,this.width,this.height) 
@@ -164,8 +164,8 @@ function start(){
     numTaco = 0
     frames = 0
     score =0
-    velocidad = 1
-    tacosTurnos=[]
+    // velocidad = 1
+    // turnos=[]
     tRex = new Trex()
     if(!interval) interval = setInterval(update,1000/60)
     audioBack.src = "./Audio/quiero_tacos.mp3"
@@ -202,8 +202,8 @@ function gameOver(){
     clearInterval(interval)
     interval = null
     ctx.fillStyle = "white"
-    ctx.font = "bold 80px Monaco"
-    ctx.fillText("GAME OVER", 250,150)
+    ctx.font = "bold 100px Monaco"
+    ctx.fillText("GAME OVER", 240,140)
     // ctx.fillStyle = "black"
     // ctx.font = "bold 40px Monaco"
     // ctx.fillText("Tacos: " + numTaco ,360,250)
@@ -214,20 +214,24 @@ function gameOver(){
     var audio = new Audio(src = "./Audio/mis_tacos.mp3")
     audio.play()
     turnos.push(numTaco)
+    console.log(turnos[0])
     if(turnos.length === 2){
-        ctx.fillStyle = "black"
+        ctx.fillStyle = "white"
         ctx.font = "bold 40px Monaco"
-        ctx.fillText("Jugador 1-- Tacos " + turnos[0] ,160,250)
-        ctx.fillText("Jugador 2 -- Tacos " + turnos[1] ,160,320)
+        ctx.fillText("Jugador 1 -- Tacos " + turnos[0] ,270,230)
+        ctx.fillText("Jugador 2 -- Tacos " + turnos[1] ,270,320)
+        ctx.fillStyle = "red"
         ctx.font = "bold 20px Monaco"
-        ctx.fillText("Presiona 'Return' para reiniciar", 260,360)
+        ctx.fillText("Presiona 'Return' para reiniciar", 350,360)
     } else{
         ctx.fillStyle = "black"
         ctx.font = "bold 40px Monaco"
-        ctx.fillText("Jugador 1 -- Tacos " + turnos[0] ,170,250)
+        ctx.fillText("Jugador 1 -- Tacos " + turnos[0] ,270,230)
+        ctx.fillStyle = "red"
         ctx.font = "bold 20px Monaco"
-        ctx.fillText("Presiona 'Return' para jugador 2", 260,360)
+        ctx.fillText("Presiona 'Return' para jugador 2", 350,360)
     }
+
     
 }
 
@@ -252,7 +256,7 @@ function generarRoca(){
             obstaculos.push(new Obstaculos (y, images.obstaculo_carro1, 50, 50))
         }
     }
-    if (frames%223===0){
+    if (frames%211===0){
         if (score<=100){
             var y = Math.floor(Math.random()*60 + 300)
             obstaculos.push(new Obstaculos(y, images.obstaculo_tronco))
@@ -273,7 +277,7 @@ function drawRocas(){
 }
 
 function generarTacos(){
-    if(frames%337===0){
+    if(frames%307===0){
         var y = Math.floor(Math.random()*60 + 300)
         tacos.push(new Tacos(y))
     }
